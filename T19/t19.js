361cfdbcc6180763bb88a79b41099c75cc501769
Nived Damodaran <94703248+N1vedd@users.noreply.github.com>
2021-11-22 03:55:22 +0530

    Update t19.js

--- T19/t19.js
+++ T19/t19.js
@@ -1,6 +1,11 @@
 let search = document.querySelector("#search");
+let back = document.querySelector(".back--2");
 let page = document.querySelector(".slide--1");
 
 search.addEventListener('click',function(){
-	page.classList.add("sliding");
+	page.classList.add("sliding-front");
+});
+
+back.addEventListener('click',function(){
+	page.classList.remove("sliding-front");
 });
